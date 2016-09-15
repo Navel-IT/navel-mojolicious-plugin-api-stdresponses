4148f4ebf6952a610e1a2c06ea5d4b0731f2f1cb
le-garff-yoann <pe.weeble@yahoo.fr>
2016-09-15 18:43:14 +0200

    update copyright

--- lib/Navel/Mojolicious/Plugin/Swagger2/StdResponses.pm
+++ lib/Navel/Mojolicious/Plugin/Swagger2/StdResponses.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2015 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
+# Copyright (C) 2015-2016 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
 # navel-mojolicious-plugin-swagger2-stdresponses is licensed under the Apache License, Version 2.0
 
 #-> BEGIN
@@ -98,7 +98,7 @@ Navel::Mojolicious::Plugin::Swagger2::StdResponses
 
 =head1 COPYRIGHT
 
-Copyright (C) 2015 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
+Copyright (C) 2015-2016 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
 
 =head1 LICENSE
 
